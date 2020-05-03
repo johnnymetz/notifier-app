6692d144d3217d692edaaf89e2c0e03ea5b8fa76
Jonathan Metz <jcmetz21@gmail.com>
2020-05-03 15:09:39 -0700

    Staticfiles tinkering

--- api/api/settings/base.py
+++ api/api/settings/base.py
@@ -97,11 +97,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 # https://devcenter.heroku.com/articles/django-assets
 
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
 # Celery
