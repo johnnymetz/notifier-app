4043796af38411c2c3e56389369dcb5676c7530b
Jonathan Metz <jcmetz21@gmail.com>
2020-05-03 13:57:47 -0700

    Serve staticfiles with whitenoise

--- api/api/settings/base.py
+++ api/api/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -95,7 +96,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 # Celery
