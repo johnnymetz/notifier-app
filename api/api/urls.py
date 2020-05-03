1cde6389cd832eb37d9915b3a5bcc72906643419
Jonathan Metz <jcmetz21@gmail.com>
2020-05-03 13:41:28 -0700

    Use api image for all celery instances. Remove notifier api for now until there's auth around it.

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from .views import welcome
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("notifier.urls")),
+    path("", welcome)
+    # path("", include("notifier.urls")),
 ]
