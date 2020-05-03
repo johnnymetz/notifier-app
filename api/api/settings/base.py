b1eb31e313cf9475ba98fe7a089489e1ed628e45
Jonathan Metz <jcmetz21@gmail.com>
2020-05-03 14:29:31 -0700

    Use static files settings specififed in heroku article

--- api/api/settings/base.py
+++ api/api/settings/base.py
@@ -95,10 +95,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://devcenter.heroku.com/articles/django-assets
 
-STATIC_URL = "/staticfiles/"
+STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
 # Celery
