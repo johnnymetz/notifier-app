2fa7cba4a6185283922938c6c8ab96457e23efb2
Jonathan Metz <jcmetz21@gmail.com>
2020-05-30 23:18:33 -0700

    Fix admin url

--- api/api/settings/local.py
+++ api/api/settings/local.py
@@ -3,7 +3,6 @@ from .base import *
 # General
 SECRET_KEY = "12345"
 DEBUG = True
-ADMIN_URL = "admin"
 
 # Email
 # https://github.com/anymail/django-anymail (try this?)
--- api/api/urls.py
+++ api/api/urls.py
@@ -1,11 +1,12 @@
-from django.conf import settings
+import os
+
 from django.contrib import admin
 from django.urls import path
 
 from .views import welcome
 
 urlpatterns = [
-    path(f"{settings.ADMIN_URL}/", admin.site.urls),
+    path(f"{os.environ.get('ADMIN_URL', 'admin')}/", admin.site.urls),
     path("", welcome)
     # path("", include("notifier.urls")),
 ]
