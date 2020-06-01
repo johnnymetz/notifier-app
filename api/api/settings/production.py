532093fd57bc45736bd97cecf4c956ddbb4ce771
Jonathan Metz <jcmetz21@gmail.com>
2020-05-31 20:39:30 -0700

    Fix email host

--- api/api/settings/production.py
+++ api/api/settings/production.py
@@ -30,7 +30,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("SENDGRID_USERNAME")
 EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_PASSWORD")
