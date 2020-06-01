f8f68496be078a698af894400ec0ec38f2a7fe13
Jonathan Metz <jcmetz21@gmail.com>
2020-05-31 22:33:46 -0700

    Better env var names

--- api/api/settings/production.py
+++ api/api/settings/production.py
@@ -32,8 +32,8 @@ AUTH_PASSWORD_VALIDATORS = [
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("GMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("GMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
 # Security
