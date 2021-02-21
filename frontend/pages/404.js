cbf4df10dfe012b912c80be9c83509e24d8f43fa
Jonathan Metz <jcmetz21@gmail.com>
2021-02-20 20:44:53 -0800

    Add custom 404 page

---
+++ frontend/pages/404.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
+export default () => {
+  return (
+    <Row className="justify-content-center">
+      <Col sm={8}>
+        <Card body className="text-center">
+          <h1>404</h1>
+          <p>Page Not Found</p>
+          <div>
+            <Link href="/">
+              <a>Back to Home</a>
+            </Link>
+          </div>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
