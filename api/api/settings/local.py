2860a46a45d2764bcfe109c11725af2dba91f356
Johnny Metz <jcmetz21@gmail.com>
2022-01-15 19:36:01 -0800

    Colorize logs with Rich

--- api/api/settings/local.py
+++ api/api/settings/local.py
@@ -16,3 +16,17 @@ SIMPLE_JWT = {
 
 # django-templated-mail
 DOMAIN = "localhost:3001"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {"rich": {"datefmt": "[%X]"}},
+    "handlers": {
+        "console": {
+            "class": "rich.logging.RichHandler",
+            "formatter": "rich",
+            "level": "DEBUG",
+        }
+    },
+    "loggers": {"django": {"handlers": ["console"]}},
+}
