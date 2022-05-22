290bf756bffa69a94c0ec984ffda62b3c5bd0516
Jonathan Metz <jcmetz21@gmail.com>
2022-05-21 22:32:30 -0700

    Add gunicorn config to prod

--- api/api/gunicorn_conf.py
+++ api/api/gunicorn_conf.py
@@ -1,7 +1,14 @@
-# Gunicorn configuration file
-# https://docs.gunicorn.org/en/stable/configure.html#configuration-file
-# https://docs.gunicorn.org/en/stable/settings.html
-# https://adamj.eu/tech/2021/12/29/set-up-a-gunicorn-configuration-file-and-test-it/
+"""
+Gunicorn configuration file
+https://docs.gunicorn.org/en/stable/configure.html#configuration-file
+https://docs.gunicorn.org/en/stable/settings.html
+https://adamj.eu/tech/2021/12/29/set-up-a-gunicorn-configuration-file-and-test-it/
+
+Can verify config with: gunicorn api.wsgi:application --config python:gunicorn_conf --print-config
+
+I tried putting this file in it's expected location (./gunicorn.conf.py)
+but it broke `make pytest`.
+"""
 import multiprocessing
 
 max_requests = 1000
