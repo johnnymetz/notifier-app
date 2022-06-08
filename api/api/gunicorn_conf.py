5802a06c880c9d0c317b81aeb5d90e9615e1a263
Jonathan Metz <jcmetz21@gmail.com>
2022-06-08 11:31:50 -0700

    Use 1 gunicorn worker to prevent OOM issues

--- api/api/gunicorn_conf.py
+++ api/api/gunicorn_conf.py
@@ -4,18 +4,26 @@ https://docs.gunicorn.org/en/stable/configure.html#configuration-file
 https://docs.gunicorn.org/en/stable/settings.html
 https://adamj.eu/tech/2021/12/29/set-up-a-gunicorn-configuration-file-and-test-it/
 
-Can verify config with: gunicorn api.wsgi:application --config python:gunicorn_conf --print-config
+Can verify config with: gunicorn api.wsgi:application --config python:api.gunicorn_conf --print-config
 
 I tried putting this file in it's expected location (./gunicorn.conf.py)
 but it broke `make pytest`.
 """
-import multiprocessing
+# import multiprocessing
 
 max_requests = 1000
 max_requests_jitter = 50
 
 log_file = "-"
 
-workers = multiprocessing.cpu_count() * 2 + 1
+# This created 17 workers on heroku which lead to OOM issues.
+# 1 worker default is fine for now.
+# workers = multiprocessing.cpu_count() * 2 + 1
 
-timeout = 20
+# Heroku has a 30 sec request timeout: https://devcenter.heroku.com/articles/request-timeout
+# We want to set a gunicorn timeout well below the Heroku timeout so we don't
+# create a situation outlined in the Heroku docs:
+# While the router has returned a time out response to the client, your application
+# will not know that the request it is processing  has reached a time-out, and your
+# application will continue to work on the request.
+timeout = 15
