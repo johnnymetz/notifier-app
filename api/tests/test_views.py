d313e2723e23790b5d2ca9d2611b0764d64e2eec
Jonathan Metz <jcmetz21@gmail.com>
2023-02-27 00:23:36 -0500

    Remove unused param

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 
 
-def test_welcome_view(client, settings):
+def test_welcome_view(client):
     url = reverse("welcome")
     r = client.get(url)
     assert r.status_code == status.HTTP_200_OK
