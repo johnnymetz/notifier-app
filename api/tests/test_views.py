5752d32e0f9c56cbfc388612ae45811b2368be8d
Jonathan Metz <jcmetz21@gmail.com>
2023-02-26 23:32:41 -0500

    Ensure debug

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 from rest_framework import status
 
 
-def test_welcome_view(client):
+def test_welcome_view(client, settings):
+    assert settings.DEBUG is True
     url = reverse("welcome")
     r = client.get(url)
     assert r.status_code == status.HTTP_200_OK
